7d0e1c2c9b473104745f3057605d38c832e51d55
leo-liuzy <zliu@cs.utexas.edu>
2026-03-04 04:10:24 +0000

    Deploying to gh-pages from @ leo-liuzy/leo-liuzy.github.io@d3861eb79421ad70c23e57ae203b79491ebd31f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -362,16 +362,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-start-new-journey-at-salesforce-as-a-research-scientist",
+          title: 'Start new journey at Salesforce as a Research Scientist! 🎉',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
